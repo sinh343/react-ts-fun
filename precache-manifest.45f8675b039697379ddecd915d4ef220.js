78078ad683558989c3ac902a1f1467480ef9cb74
sinh343 <yosefsuga@hotmail.co.uk>
2020-09-26 20:33:00 +0100

    Updates

---
+++ precache-manifest.45f8675b039697379ddecd915d4ef220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8911e9665cbe39e14ee008fda963aa8",
+    "url": "/react-ts-fun/index.html"
+  },
+  {
+    "revision": "5708fa6e153739929478",
+    "url": "/react-ts-fun/static/css/main.f40bec73.chunk.css"
+  },
+  {
+    "revision": "ed891ec2150e30b95ee0",
+    "url": "/react-ts-fun/static/js/2.cdc0dc9a.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/react-ts-fun/static/js/2.cdc0dc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5708fa6e153739929478",
+    "url": "/react-ts-fun/static/js/main.9277b84c.chunk.js"
+  },
+  {
+    "revision": "173220a084fcd7e76db0",
+    "url": "/react-ts-fun/static/js/runtime-main.a30e878f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-ts-fun/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/react-ts-fun/static/media/pokeball.2b4798f5.svg"
+  }
+]);
